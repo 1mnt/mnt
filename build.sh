366d598a18584ce7e9aba051f4a1a2fb765608ed
1mnt <1mnt@users.noreply.github.com>
2026-02-23 04:06:54 +0700

    (#12) 2026-02-23 04:06:54

--- build.sh
+++ build.sh
@@ -24,6 +24,44 @@ build_src() {
     mka bacon
 }
 
+upload_rom() {
+    set -x
+
+    release_file=$(find out/target/product -name "*-RMX*.zip" -print -quit)
+    release_name=$(basename "$release_file" .zip)
+    release_tag=$(date +%Y%m%d)
+    repo_releases="bimuafaq/releases"
+    UPLOAD_GH=false
+
+    if [[ -f "$release_file" ]]; then
+        if [[ "${UPLOAD_GH}" == "true" && -n "$GITHUB_TOKEN" ]]; then
+            echo "$GITHUB_TOKEN" | gh auth login --with-token
+            rovx --post "Uploading to GitHub Releases..."
+            gh release create "$release_tag" -t "$release_name" -R "$repo_releases" -F "rovx/script/notes.txt" || true
+            
+            if gh release upload "$release_tag" "$release_file" -R "$repo_releases" --clobber; then
+                rovx --post "GitHub Release upload successful: <a href='https://github.com/$repo_releases/releases/tag/$release_tag'>$release_name</a>"
+            else
+                rovx --post "GitHub Release upload failed"
+            fi
+        fi
+
+        mkdir -p ~/.config
+        unzip -q "$PWD/config.zip" -d ~/.config
+        rovx --post "Uploading build result to Telegram..."
+
+        if timeout 15m telegram-upload "$release_file" --to "$TG_CHAT_ID" --caption "$CIRRUS_COMMIT_MESSAGE"; then
+            rovx --post "Telegram upload successful"
+        else
+            rovx --post "telegram-upload failed"
+            exit 1
+        fi
+    else
+        rovx --post "Build file not found"
+        exit 0
+    fi
+}
+
 main() {
     case "${1:-}" in
         -s|--sync)
@@ -32,11 +70,15 @@ main() {
         -b|--build)
             build_src
             ;;
+        -u|--upload)
+            upload_rom
+            ;;
         *)
             echo "Usage: ./build.sh [FLAGS]"
             echo "Options:"
             echo "  -s, --sync      Sync source"
             echo "  -b, --build     Start build process"
+            echo "  -u, --upload    Upload build"
             return 1
             ;;
     esac
