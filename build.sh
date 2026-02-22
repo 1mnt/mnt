5c50de3999d14b1771909ec224ad3d520d33088a
1mnt <1mnt@users.noreply.github.com>
2026-02-22 15:26:27 +0700

    (#6) 2026-02-22 15:26:27

--- build.sh
+++ build.sh
@@ -13,39 +13,51 @@ setup_src() {
 }
 
 build_src() {
+    source build/envsetup.sh
     if [[ ! -d "reclient" ]]; then
         git clone -q https://github.com/rovars/reclient
     fi
 
-    unset USE_CCACHE CCACHE_EXEC USE_GOMA
-    unset RBE_use_rpc_credentials
-    unset RBE_service_no_auth
+    cat > "$PWD/reclient_helper.sh" << 'EOF'
+#!/bin/bash
+cat << HELPER
+{
+  "headers": {
+    "x-buildbuddy-api-key": ["${RBE_API_KEY}"]
+  },
+  "token": "dummy"
+}
+HELPER
+EOF
+    chmod +x "$PWD/reclient_helper.sh"
 
-    export CCACHE_DIR="/tmp/ccache"
-
+    export ANDROID_BUILD_ENVIRONMENT_CONFIG="buildbuddy_rbe"
+    export ANDROID_BUILD_ENVIRONMENT_CONFIG_DIR="build/soong/docs"
+    export RBE_service="rovx.buildbuddy.io:443"
+    export RBE_instance="default"
+    export RBE_automatic_auth="false"
+    export RBE_use_gce_credentials="false"
+    export RBE_use_application_default_credentials="false"
+    export RBE_use_rpc_credentials="true"
+    export RBE_credentials_helper="$PWD/reclient_helper.sh"
+    export RBE_cas_concurrency="100"
+    export RBE_use_unified_cas_ops="true"
+    export RBE_use_unified_downloads="true"
+    export RBE_use_unified_uploads="true"
+    export RBE_use_batches="true"
+    export RBE_cas_rpc_timeout="10m"
+    export RBE_compression_threshold="100"
+    export RBE_noremote_upload_local_results="true"
     export USE_RBE="1"
     export RBE_DIR="$PWD/reclient"
     export RBE_exec_root="$PWD"
-
     export RBE_CXX_EXEC_STRATEGY="remote"
     export RBE_JAVAC_EXEC_STRATEGY="remote"
     export RBE_R8_EXEC_STRATEGY="remote"
     export RBE_D8_EXEC_STRATEGY="remote"
-
     export RBE_JAVAC="1"
     export RBE_R8="1"
     export RBE_D8="1"
-
-    export RBE_use_unified_cas_ops="true"
-    export RBE_use_unified_downloads="true"
-    export RBE_use_unified_uploads="true"
-
-    export RBE_use_application_default_credentials="false"
-    export RBE_use_gce_credentials="false"
-
-    export RBE_instance="rovx.buildbuddy.io"
-    export RBE_service="rovx.buildbuddy.io:443"
-    export RBE_remote_headers="$ROVBE"
 
     local rbex_logs="/tmp/rbelogs"
     mkdir -p "$rbex_logs"
@@ -56,12 +68,10 @@ build_src() {
     export OWN_KEYS_DIR="$PWD/rox/keys"
     sudo ln -sf "$OWN_KEYS_DIR/releasekey.pk8" "$OWN_KEYS_DIR/testkey.pk8"
     sudo ln -sf "$OWN_KEYS_DIR/releasekey.x509.pem" "$OWN_KEYS_DIR/testkey.x509.pem"
-
-    source build/envsetup.sh
+   
     lunch lineage_RMX2185-user
     mka bacon
 }
-
 
 main() {
     case "${1:-}" in
