ce8b93cd83e228b1fe6634a0964efde931d38819
1mnt <1mnt@users.noreply.github.com>
2026-03-05 07:48:52 +0700

    (#118) 2026-03-05 07:48:52

--- build.sh
+++ build.sh
@@ -8,8 +8,7 @@ setup_src() {
 
     repo sync -j8 -c --no-clone-bundle --no-tags
 
-    # sed -i 's/\$(error SELINUX_IGNORE_NEVERALLOWS/\$(warning SELINUX_IGNORE_NEVERALLOWS/g' system/sepolicy/Android.mk
-    #patch -p1 < "$PWD/rox/script/permissive_se.patch"
+    patch -p1 < "$PWD/rox/script/permissive_se.patch"
     source "$PWD/rox/script/constify.sh"
 
     git clone https://github.com/bimuafaq/android_vendor_extra vendor/extra
@@ -20,8 +19,8 @@ setup_src() {
     #git reset --hard HEAD~3
     #cd -
 
-    rm -rf device/realme/RMX2185
-    git clone https://github.com/rovars/device_realme_RMX2185 device/realme/RMX2185 --depth=1
+    #rm -rf device/realme/RMX2185
+    #git clone https://github.com/rovars/device_realme_RMX2185 device/realme/RMX2185 --depth=1
 }
 
 build_src() {
@@ -35,8 +34,8 @@ build_src() {
     lunch lineage_RMX2185-user
     # source "$PWD/rox/script/mmm.sh" icons
     chmod +x "$PWD/rox/script/fix.sh"
-    source "$PWD/rox/script/fix.sh" || exit 1
-    #mka bacon
+    #source "$PWD/rox/script/fix.sh" || exit 1
+    mka bacon
     #mka selinux_policy
 }
 
