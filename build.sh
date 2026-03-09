a3d6f560246fbea2bd30066380394094457a3fc1
1mnt <1mnt@users.noreply.github.com>
2026-03-10 05:24:08 +0700

    (#131) 2026-03-10 05:24:08

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ setup_src() {
 }
 
 build_src() {
-    source "$PWD/build/envsetup.sh"
+    source "$PWD/build/rbesetup.sh"
     # source rovx --ccache
 
     export RBE_use_application_default_credentials="false"
