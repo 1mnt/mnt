1389cbdec837d7ee5dc5e47d9d1fb6b6b787080e
1mnt <1mnt@users.noreply.github.com>
2026-03-10 13:57:27 +0000

    (#56) 2026-03-10 13:57:27

--- build.sh
+++ build.sh
@@ -100,14 +100,13 @@ v8_symbol_level = 0
 blink_symbol_level = 0
 EOF
 
-    export RBE_EXEC_STRATEGY=remote_local_fallback
+    export RBE_exec_strategy=remote_local_fallback
+    export RBE_num_remote_reruns=3
     
     gn gen out/Default
     mkdir -p out
     
-    # timeout 30m siso ninja --offline -C out/Default chrome_public_apk 2>&1 | tee out/error.log || true
-
-    siso ninja -k 0 -C out/Default chrome_public_apk 2>&1 | tee -a out/error.log
+    siso ninja -k 0 -C out/Default chrome_public_apk 2>&1 | tee out/error.log
 }
 
 upload_build() {
